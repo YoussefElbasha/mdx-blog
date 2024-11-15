8c440b919ba205f083d93359819f1842517abee7
Youssef Saad <youssef.elbasha.saad@gmail.com>
2024-11-15 20:29:01 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link'
 const Home = async () => {
   const blogs = await fs.readdir(process.cwd() + `/src/blogs`, 'utf8')
 
+  console.log(blogs)
+  console.log(fs.readFile(process.cwd() + `/src/blogs/test.md`))
+
   return (
     <div className='flex flex-col justify-start items-center'>
       {blogs.map((blog: string) => {
